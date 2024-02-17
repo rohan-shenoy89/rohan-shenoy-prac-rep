1d7d89c688305662a0b3e79c49d309cacd820480
Rohan Shenoy <101130822+rshen12@users.noreply.github.com>
2024-02-17 16:14:08 +0530

    Developer - Rohan Shenoy Date      - 17th Feb 2024 Comment   - Created the required folders for Practice and have initiated adding the details about the practice done each day referring to the date above since the git was integrated since 17/02/2024

--- 2.Py-Practice/1.Practice-1-Py-PlayGround.py
+++ 2.Py-Practice/1.Practice-1-Py-PlayGround.py
@@ -15,3 +15,23 @@ def factorial(n):
 
 
 print(f"The value for given Factorial - {factorial(11)}")        # Call the function to get the result
+print("\n*****************\n")
+
+#Function to Check for Palindrome
+
+def palindrome(word):
+    word = word.lower()     # Make sure the all the letters are in lower case
+
+    for s in range(len(word)//2): # Take the length of the word and round it up with base
+       if word[s] != word[-s -1]: # Check if 1st letter is not eq to last letter
+           return False           # if yes, return False
+    return True                   # Else return True
+
+
+palindrome('Radar')               # Take the value in the function:
+if True:                          # Check the functions returned value and print your statement
+    print(f"The given word is a Palindrome")
+else:
+    print(f"The given word is not a Palindrome")
+print("\n*****************\n")
+
